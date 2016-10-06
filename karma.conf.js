ed962f23a6d479123cc445ccdb82014fe18c12fe
George Witte <geowittejr@gmail.com>
2016-10-06 00:27:28 -0400

    Testing Travis

--- karma.conf.js
+++ karma.conf.js
@@ -6,9 +6,6 @@ module.exports = function(config) {
 
   var configuration = {
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
