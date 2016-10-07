6047c502d5e967aa64fcc6a91a431e5d33e13139
George Witte <geowittejr@gmail.com>
2016-10-06 23:26:46 -0400

    Fixed ts warnings in spec file. Added jasmine type def

--- test/client/app/app.spec.tsx
+++ test/client/app/app.spec.tsx
@@ -1,9 +1,14 @@
 
-describe("A suite is just a function", function() {
-    var a;
+describe("We're running a test here...", function() {
 
-    it("and so is a spec", function() {
-        a = true;
+    it("and true means true", function() {
+        let a : boolean = true;
         expect(a).toBe(true);
     });
+
+    it("and 1 does not equal 2", function() {
+        let b : number = 1;
+        expect(b).not.toEqual(2);
+    });
+
 });
